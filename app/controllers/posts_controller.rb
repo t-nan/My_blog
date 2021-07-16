e475ddc6849bb765d4594b5ea2c63e5b8e232dcd
t-nan <t-nan@yandex.ru>
2021-07-16 17:54:13 +0300

    add controller Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts=Post.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+
+  def post_params
+    params.require(:post).permit(:author,:body)
+  end
+
+end
