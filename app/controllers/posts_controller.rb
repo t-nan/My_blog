15bde21a5dfda90aaace64cc3d45b093fabe2e04
t-nan <t-nan@yandex.ru>
2021-07-17 14:37:04 +0300

    add edit,update,destroy methods to post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,21 +16,35 @@ class PostsController < ApplicationController
     @post=Post.new(post_params) 
 
     if @post.save
-      redirect_to posts_path
+      redirect_to posts_path, notice: "Your question has been saved!"
     else
       @error=@post.errors.full_messages
-      render 'new'
+      render 'new'      
     end
 
   end
 
   def edit
+    @post=Post.find(params[:id])
   end
 
   def update
+
+    @post=Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      @error=@post.errors.full_messages
+      render 'edit'
+    end
+
   end
 
   def destroy
+    @post=Post.find(params[:id])
+    @post.delete
+    redirect_to posts_path
   end
 
 
