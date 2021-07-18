7fb8dcb89bc08f4299081e1371b4faf48663d07e
t-nan <t-nan@yandex.ru>
2021-07-18 16:05:48 +0300

    add Comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,42 @@
+class CommentsController < ApplicationController
+
+  def index   
+  end
+
+  def show
+  end
+
+  def new
+    @post=Post.find(params[:post_id])
+  end
+
+
+  def create
+    @post=Post.find(params[:post_id])
+    @comment=@post.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to post_path(@post), notice: "Your comment has been saved!"
+    else
+      redirect_to post_path, alert: "Comment not saved ! Make sure that the forms are filled in correctly."
+    end
+
+  end
+
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author,:body)
+  end
+end
