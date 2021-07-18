1ff87ee85f0d97ff6b1d78574af30519ee03fedf
t-nan <t-nan@yandex.ru>
2021-07-18 16:05:29 +0300

    add migration for Comment

---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CommentsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index,new,show,edit" do
+    get comments_index,new,show,edit_url
+    assert_response :success
+  end
+
+end
