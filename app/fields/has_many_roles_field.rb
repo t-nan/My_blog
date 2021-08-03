d96f38e2f81e40956ae03983747090c44de8df1e
t-nan <t-nan@yandex.ru>
2021-08-03 12:03:48 +0300

    add validation & class HasManyRolesField

---
+++ app/fields/has_many_roles_field.rb
@@ -0,0 +1,5 @@
+require "administrate/field/base"
+
+  class HasManyRolesField < Administrate::Field::HasMany
+  end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,26 @@
 class User < ApplicationRecord
+  after_initialize :set_default_role, if: :new_record?
+
+  rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
 
-  validates :username, uniqueness: true       
+  validates :username, uniqueness: true
+  validates :roles, presence: true     
 
   has_many :posts
   has_many :comments       
 
   acts_as_voter
+
+
+  
+  def set_default_role
+    self.add_role(:normal)
+  end
 
          
 end
