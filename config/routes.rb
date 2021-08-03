0c8f200686383ae6f388e2d7bb391e95de646589
t-nan <t-nan@yandex.ru>
2021-08-03 11:59:24 +0300

    administrate gem generates admin controllers & routes to admin url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
  
+  namespace :admin do
+      resources :users
+      resources :comments
+      resources :posts
+      resources :roles, only: [:index, :show]
+
+      root to: "users#index"
+    end
   devise_for :users
  
 
