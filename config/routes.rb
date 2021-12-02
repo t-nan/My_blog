f59b95bc656c1283d0d1c7f6e39cb7410078ddc5
t-nan <t-nan@yandex.ru>
2021-12-02 22:22:36 +0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,28 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
- 
   namespace :admin do
-      resources :users
-      resources :comments
-      resources :posts
-      resources :roles, only: [:index, :show]
-
-      root to: "users#index"
-    end
-  devise_for :users
- 
-
-   root to: "posts#index"
-
     resources :users
+    resources :comments
+    resources :posts
+    resources :roles, only: %i[index show]
 
-    resources :posts do
-      resources :comments
+    root to: 'users#index'
+  end
+  devise_for :users
+
+  root to: 'posts#index'
+
+  resources :users
+
+  resources :posts do
+    resources :comments
+  end
+
+  resources :posts do
+    member do
+      patch 'upvote', to: 'posts#upvote'
+      patch 'downvote', to: 'posts#downvote'
     end
-
-    resources :posts do
-      member do
-        patch "upvote", to: "posts#upvote"
-        patch "downvote", to: "posts#downvote"
-      end
-    end
-
+  end
 end
-
-
-
-
-  
