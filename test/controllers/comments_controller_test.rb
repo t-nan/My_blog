385c9732ec9cddaf869dd957097c3f744f3101fb
t-nan <t-nan@yandex.ru>
2021-12-02 22:21:58 +0300

    test

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index,new,show,edit" do
-    get comments_index,new,show,edit_url
+  test 'should get index,new,show,edit' do
+    get comments_index, new, show, edit_url
     assert_response :success
   end
-
 end
